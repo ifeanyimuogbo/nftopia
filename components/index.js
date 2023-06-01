3974c81e1631b4a38140383e9cdea59eee8fc190
ifeanyimuogbo <ifeanyichukwumuogbo@gmail.com>
2023-06-01 01:45:22 +0100

    Initial commit

---
+++ components/index.js
@@ -0,0 +1,16 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DeatilsBid";
+
+export {
+  FocusedStatusBar,
+  HomeHeader,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+};
